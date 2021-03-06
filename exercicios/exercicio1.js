51f2a27ce93d5e98ba9cc0ee3aa360116cca94cc
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2021-03-05 21:14:01 -0300

    Exercício 1 Concluido

---
+++ exercicios/exercicio1.js
@@ -0,0 +1,10 @@
+function calcularTotal(valor1, valor2) {
+    console.log(valor1+valor2);
+    console.log(valor1-valor2);
+    console.log(valor1*valor2);
+    console.log(valor1/valor2);
+}
+
+calcularTotal(8, 10);
+
+//01 Concluido
